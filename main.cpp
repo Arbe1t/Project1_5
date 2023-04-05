fd9d05003aa24914969491e96160bc7545375e5f
Arbe1t <kostabrizak@gmail.com>
2023-04-05 10:36:10 +0300

    big bong

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float x, y, k;
+cout << "calculator" << endl;
+cout << "enter number: " << endl;
+cin >> x;
+cout << "enter number: " << endl;
+cin >> y;
+cout << "enter number: " << endl;
+cin >> k;
+cout << "dobutok= " << x*y*k << endl;
+cout << "suma= " << x+y+k << endl;
+
+return 0;
+}
